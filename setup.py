be2ab5a980887e537ac279c8c80b0d2fbcb5d122
Sebastp desktop app <sebastianpob@gmail.com>
2020-01-22 15:03:01 +0100

    setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Stonks",
+    version="0.1",
+    author="Sebastian Pobocha",
+    description="This is an Example Package",
+    keywords="stonks",
+    url="https://github.com/Sebastp/Stonks-python",
+    install_requires=[""],
+    packages=[],
+    scripts=[""],
+)
